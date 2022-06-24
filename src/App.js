a2e45bf7f2d095674064e3371c11726582e614a9
Zaiza2000 <69431042+Zaiza2000@users.noreply.github.com>
2022-06-24 16:15:52 +0700

    Implement Loading Button

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ export default function App() {
   const [order, setOrder] = useState("");
   const [name, setName] = useState("");
   const [userLineID, setUserLineID] = useState("");
+  const [loading, setLoading] = useState(false);
 
   async function fetchUserData() {
     await liff
@@ -24,9 +25,10 @@ export default function App() {
         console.log(err.code, err.message);
         liff.closeWindow();
       });
+    
   }
-// TODO: LIFF Must Be Closed Faster to Sent receive order to user
-  async function receiveOrder(userId,order) {
+  // TODO: LIFF Must Be Closed Faster to Sent receive order to user
+  async function receiveOrder(userId, order) {
     try {
       await axios.post("https://woxa-food-order.herokuapp.com/receive-order", {
         id: userId,
@@ -44,11 +46,13 @@ export default function App() {
 
   const handleOrderChange = (e) => {
     setOrder(e.target.value);
+    
   };
 
   //write
   const writeToDatabase = (e) => {
     e.preventDefault();
+    setLoading(true);
     const timestamp = Date.now();
     set(ref(db, `/${timestamp}`), {
       order: order,
@@ -56,7 +60,7 @@ export default function App() {
       name: name,
       time: timestamp
     });
-    receiveOrder(userLineID,order);
+    receiveOrder(userLineID, order);
     setOrder("");
   };
 
@@ -67,7 +71,6 @@ export default function App() {
           <h1 className="text-2xl text-blue-700 font-bold sm:text-3xl">
             WOXA KITCHEN
           </h1>
-
           <p className="mt-4 text-gray-500">สั่งอาหารได้เลย</p>
         </div>
 
@@ -82,11 +85,18 @@ export default function App() {
                 className="w-full p-4 pr-12 text-sm border-2  rounded-lg  bg-gray-100 shadow-sm"
                 placeholder="กะเพราทะเล + ไข่ดาว"
               />
+
               <button
-                className="inline-block mt-5 px-5 py-3 ml-3 text-sm font-medium text-white bg-blue-500 rounded-lg"
+                className="inline-block w-full mt-5 px-5 py-3 text-sm font-medium text-white bg-blue-500 rounded-lg"
                 onClick={writeToDatabase}
+                disabled={loading}
               >
-                สั่งอาหาร
+                {loading && (<svg role="status" className="w-6 h-6 mr-2 text-gray-200 inline animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                </svg>)}
+                {loading && <span>กำลังสั่งอาหาร</span>}
+                {!loading && <span>สั่งอาหาร</span>}
               </button>
             </div>
           </div>
